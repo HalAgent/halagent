161fc8bcd4160c3a9b522c99317d3fd6dbb47f68
DeBiao-Ao <zcy34303@gmail.com>
2025-02-15 21:19:52 +0800

    extension floatbtn

---
+++ halui/extension/contents/floatBtn/index.tsx
@@ -0,0 +1,26 @@
+import cssText from "data-text:~/contents/floatBtn/index.css"
+
+import HAlphaImg from "raw:~/assets/HAlpha.svg"
+import LogoImg from "raw:~/assets/logo.svg"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"]
+}
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
+const FloatBtn = () => {
+    console.warn(111);
+
+  return (
+    <div className="hal-float-btn">
+      <img src={LogoImg} className="hal-float-btn-icon"></img>
+      <img src={HAlphaImg} className="hal-float-btn-text"></img>
+    </div>
+  )
+}
+
+export default FloatBtn
